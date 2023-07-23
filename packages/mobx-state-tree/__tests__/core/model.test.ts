e0999738c6cb419bf985f9eaf41e927034378591
Tyler Williams <tyler@coolsoftware.dev>
2023-07-22 22:23:05 -0600

    spike: early ideas

--- packages/mobx-state-tree/__tests__/core/model.test.ts
+++ packages/mobx-state-tree/__tests__/core/model.test.ts
@@ -1,4 +1,5 @@
 import { applySnapshot, getSnapshot, types } from "../../src"
+import { Hook } from "../../src/internal"
 
 test("it should call preProcessSnapshot with the correct argument", () => {
     const onSnapshot = jest.fn((snapshot: any) => {
@@ -17,152 +18,206 @@ test("it should call preProcessSnapshot with the correct argument", () => {
     applySnapshot(model, { val: 1 })
     expect(onSnapshot).lastCalledWith({ val: 1 })
 })
-describe("Model name", () => {
-    test("Providing a string as the first argument should set it as the model's name.", () => {
-        const Model = types.model("Name", {})
+describe("Model instantiation", () => {
+    describe("Model name", () => {
+        test("Providing a string as the first argument should set it as the model's name.", () => {
+            const Model = types.model("Name", {})
 
-        expect(Model.name).toBe("Name")
+            expect(Model.name).toBe("Name")
+        })
+        test("Providing an empty string as the first argument should set it as the model's name.", () => {
+            const Model = types.model("", {})
+
+            expect(Model.name).toBe("")
+        })
+        describe("Providing a non-string argument as the first argument should set the model's name as 'AnonymousModel'.", () => {
+            const testCases = [
+                {},
+                null,
+                undefined,
+                1,
+                true,
+                [],
+                function () {},
+                new Date(),
+                /a/,
+                new Map(),
+                new Set(),
+                Symbol(),
+                new Error(),
+                NaN,
+                Infinity
+            ]
+
+            testCases.forEach((testCase) => {
+                test(`Providing ${JSON.stringify(
+                    testCase
+                )} as the first argument should set the model's name as 'AnonymousModel'.`, () => {
+                    const Model = types.model(testCase as any)
+
+                    expect(Model.name).toBe("AnonymousModel")
+                })
+            })
+        })
     })
-    test("Providing an empty string as the first argument should set it as the model's name.", () => {
-        const Model = types.model("", {})
+    describe("Model properties", () => {
+        test("Providing a string as the first argument and an object as the second argument should use the object's properties in the model.", () => {
+            const Model = types.model("name", {
+                prop1: "prop1",
+                prop2: 2
+            })
 
-        expect(Model.name).toBe("")
+            expect(Model.properties).toHaveProperty("prop1")
+            expect(Model.properties).toHaveProperty("prop2")
+        })
+        test("Providing an object as the first argument should parse and use its properties.", () => {
+            const Model = types.model({
+                prop1: "prop1",
+                prop2: 2
+            })
+
+            expect(Model.properties).toHaveProperty("prop1")
+            expect(Model.properties).toHaveProperty("prop2")
+        })
+        test("Providing a string as the first argument and a falsy value as the second argument should result in an empty set of properties.", () => {
+            const Model = types.model("name", null as any)
+
+            expect(Model.properties).toEqual({})
+        })
     })
-    describe("Providing a non-string argument as the first argument should set the model's name as 'AnonymousModel'.", () => {
-        const testCases = [
-            {},
-            null,
-            undefined,
-            1,
-            true,
-            [],
-            function () {},
-            new Date(),
-            /a/,
-            new Map(),
-            new Set(),
-            Symbol(),
-            new Error(),
-            NaN,
-            Infinity
-        ]
+    describe("Model identifier", () => {
+        test("If no identifier attribute is provided, the identifierAttribute should be undefined.", () => {
+            const Model = types.model("name", {})
 
-        testCases.forEach((testCase) => {
-            test(`Providing ${JSON.stringify(
-                testCase
-            )} as the first argument should set the model's name as 'AnonymousModel'.`, () => {
-                const Model = types.model(testCase as any)
+            expect(Model.identifierAttribute).toBeUndefined()
+        })
+        test("If an identifier attribute is provided, the identifierAttribute should be set for the object.", () => {
+            const Model = types.model("name", {
+                id: types.identifier
+            })
+
+            expect(Model.identifierAttribute).toBe("id")
+        })
+        test("If an identifier attribute has already been provided, an error should be thrown when attempting to provide a second one.", () => {
+            expect(() => {
+                types.model("name", {
+                    id: types.identifier,
+                    id2: types.identifier
+                })
+            }).toThrowErrorMatchingInlineSnapshot(
+                `"[mobx-state-tree] Cannot define property 'id2' as object identifier, property 'id' is already defined as identifier property"`
+            )
+        })
+    })
+    describe("Edge case behavior", () => {
+        describe("when we provide no arguments to the function", () => {
+            test("the model will be named AnonymousModel", () => {
+                const Model = types.model()
 
                 expect(Model.name).toBe("AnonymousModel")
             })
-        })
-    })
-})
-describe("Model properties", () => {
-    test("Providing a string as the first argument and an object as the second argument should use the object's properties in the model.", () => {
-        const Model = types.model("name", {
-            prop1: "prop1",
-            prop2: 2
-        })
+            test("the model will have no properties", () => {
+                const Model = types.model()
 
-        expect(Model.properties).toHaveProperty("prop1")
-        expect(Model.properties).toHaveProperty("prop2")
-    })
-    test("Providing an object as the first argument should parse and use its properties.", () => {
-        const Model = types.model({
-            prop1: "prop1",
-            prop2: 2
-        })
-
-        expect(Model.properties).toHaveProperty("prop1")
-        expect(Model.properties).toHaveProperty("prop2")
-    })
-    test("Providing a string as the first argument and a falsy value as the second argument should result in an empty set of properties.", () => {
-        const Model = types.model("name", null as any)
-
-        expect(Model.properties).toEqual({})
-    })
-})
-describe("Model identifier", () => {
-    test("If no identifier attribute is provided, the identifierAttribute should be undefined.", () => {
-        const Model = types.model("name", {})
-
-        expect(Model.identifierAttribute).toBeUndefined()
-    })
-    test("If an identifier attribute is provided, the identifierAttribute should be set for the object.", () => {
-        const Model = types.model("name", {
-            id: types.identifier
-        })
-
-        expect(Model.identifierAttribute).toBe("id")
-    })
-    test("If an identifier attribute has already been provided, an error should be thrown when attempting to provide a second one.", () => {
-        expect(() => {
-            types.model("name", {
-                id: types.identifier,
-                id2: types.identifier
+                const modelSnapshot = getSnapshot(Model.create())
+                expect(modelSnapshot).toEqual({})
             })
-        }).toThrowErrorMatchingInlineSnapshot(
-            `"[mobx-state-tree] Cannot define property 'id2' as object identifier, property 'id' is already defined as identifier property"`
-        )
-    })
-})
-describe("Edge case behavior", () => {
-    describe("when we provide no arguments to the function", () => {
-        test("the model will be named AnonymousModel", () => {
-            const Model = types.model()
-
-            expect(Model.name).toBe("AnonymousModel")
         })
-        test("the model will have no properties", () => {
-            const Model = types.model()
-
-            const modelSnapshot = getSnapshot(Model.create())
-            expect(modelSnapshot).toEqual({})
-        })
-    })
-    describe("when we provide an invalid name value, but a valid property object", () => {
-        test("the model will be named AnonymousModel", () => {
-            const Model = types.model(null as any, {
-                prop1: "prop1",
-                prop2: 2
-            })
-
-            expect(Model.name).toBe("AnonymousModel")
-        })
-        test("the model will have no properties", () => {
-            const Model = types.model(null as any, {
-                prop1: "prop1",
-                prop2: 2
-            })
-
-            const modelSnapshot = getSnapshot(Model.create())
-            expect(modelSnapshot).toEqual({})
-        })
-    })
-    describe("when we provide three arguments to the function", () => {
-        test("the model gets the correct name", () => {
-            // @ts-ignore
-            const Model = types.model("name", {}, {})
-
-            expect(Model.name).toBe("name")
-        })
-        test("the model gets the correct properties", () => {
-            const Model = types.model(
-                "name",
-                {
+        describe("when we provide an invalid name value, but a valid property object", () => {
+            test("the model will be named AnonymousModel", () => {
+                const Model = types.model(null as any, {
                     prop1: "prop1",
                     prop2: 2
-                },
-                // @ts-ignore
-                {}
-            )
+                })
 
-            const modelSnapshot = getSnapshot(Model.create())
-            expect(modelSnapshot).toEqual({
-                prop1: "prop1",
-                prop2: 2
+                expect(Model.name).toBe("AnonymousModel")
             })
+            test("the model will have no properties", () => {
+                const Model = types.model(null as any, {
+                    prop1: "prop1",
+                    prop2: 2
+                })
+
+                const modelSnapshot = getSnapshot(Model.create())
+                expect(modelSnapshot).toEqual({})
+            })
+        })
+        describe("when we provide three arguments to the function", () => {
+            test("the model gets the correct name", () => {
+                // @ts-ignore
+                const Model = types.model("name", {}, {})
+
+                expect(Model.name).toBe("name")
+            })
+            test("the model gets the correct properties", () => {
+                const Model = types.model(
+                    "name",
+                    {
+                        prop1: "prop1",
+                        prop2: 2
+                    },
+                    // @ts-ignore
+                    {}
+                )
+
+                const modelSnapshot = getSnapshot(Model.create())
+                expect(modelSnapshot).toEqual({
+                    prop1: "prop1",
+                    prop2: 2
+                })
+            })
+        })
+    })
+})
+describe("Model properties objects", () => {
+    describe("when a user names a property the same as an MST lifecycle hook", () => {
+        test("it throws an error", () => {
+            const hookValues = Object.values(Hook)
+
+            hookValues.forEach((hook) => {
+                expect(() => {
+                    types.model({
+                        [hook]: types.string
+                    })
+                }).toThrowErrorMatchingInlineSnapshot(
+                    `"[mobx-state-tree] Hook '${hook}' was defined as property. Hooks should be defined as part of the actions"`
+                )
+            })
+        })
+    })
+    describe("when a user attempts to define a property with the get keyword", () => {
+        test("it throws an error", () => {
+            expect(() => {
+                types.model({
+                    get foo() {
+                        return "bar"
+                    }
+                })
+            }).toThrowErrorMatchingInlineSnapshot(
+                `"[mobx-state-tree] Getters are not supported as properties. Please use views instead"`
+            )
+        })
+    })
+    describe("when a user attempts to define a property with null as the value", () => {
+        test("it throws an error", () => {
+            expect(() => {
+                types.model({
+                    foo: null as any
+                })
+            }).toThrowErrorMatchingInlineSnapshot(
+                `"[mobx-state-tree] The default value of an attribute cannot be null or undefined as the type cannot be inferred. Did you mean \`types.maybe(someType)\`?"`
+            )
+        })
+    })
+    describe("when a user attempts to define a property with undefined as the value", () => {
+        test("it throws an error", () => {
+            expect(() => {
+                types.model({
+                    foo: undefined as any
+                })
+            }).toThrowErrorMatchingInlineSnapshot(
+                `"[mobx-state-tree] The default value of an attribute cannot be null or undefined as the type cannot be inferred. Did you mean \`types.maybe(someType)\`?"`
+            )
         })
     })
 })
---
+++ packages/mobx-state-tree/src/types/complex-types/type-inference.md
@@ -0,0 +1,89 @@
+Explain types: https://mobx-state-tree.js.org/overview/types
+
+When we create a model in MST, we can pass in some default values and the library will infer the appropriate types. As an example:
+
+```js
+import { types } from "mobx-state-tree"
+
+const Todo = types.model({
+    name: "", // Shorthand for `types.optional(types.string, "")`
+    done: false // Shorthand for `types.optional(types.boolean, false)`
+})
+
+const User = types.model({
+    name: "" // Shorthand for `types.optional(types.string, "")`
+})
+```
+
+Input:
+
+```ts
+export interface ModelPropertiesDeclaration {
+    [key: string]: ModelPrimitive | IAnyType
+}
+```
+
+Returns:
+
+```ts
+export interface ModelProperties {
+    [key: string]: IAnyType
+}
+```
+
+First, we loop through the keys of the object. For each key:
+
+1. Check if the key is in `Hook` (what does this mean?). If so, throw an error.
+2. What does `getOwnPropertyDescriptor` do? It does that, looks for `get` to filter out getters. https://github.com/mobxjs/mobx-state-tree/issues/1700
+3. Fail if we've been given `null` or `undefined`, since we can't infer a value there. (explain a valid way to instantiate with null or undefined)
+4. For any value that isn't `null` or `undefined`, we check if a value is primitive(what does that mean) and convert it into the new type
+5. Map types and Array types get converted into reasonable defaults
+6. If `isType` comes true (how does that work?), we just return it, since it's already a `types` type.
+7. In dev mode, we will fail for the user if they passed a function
+8. In dev mode, we will fail for the user if they passed an object
+
+Hook:
+
+```ts
+export enum Hook {
+    afterCreate = "afterCreate",
+    afterAttach = "afterAttach",
+    afterCreationFinalization = "afterCreationFinalization",
+    beforeDetach = "beforeDetach",
+    beforeDestroy = "beforeDestroy"
+}
+```
+
+`getOwnPropertyDescriptor`: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/getOwnPropertyDescriptor
+
+`isPrimitive` - returns true if a value is null, undefined, string, number, boolean, or a Date instance.
+
+`isType` returns if it's an MST type, we use `isType`: packages/mobx-state-tree/src/core/type/type.ts
+
+No tests for:
+
+```ts
+throw fail(`Hook '${key}' was defined as property. Hooks should be defined as part of the actions`)
+```
+
+```ts
+throw fail("Getters are not supported as properties. Please use views instead") // Also a bug technically
+```
+
+```ts
+throw fail(
+    `Invalid type definition for property '${key}', it looks like you passed a function. Did you forget to invoke it, or did you intend to declare a view / action?`
+)
+```
+
+```ts
+throw fail(
+    `Invalid type definition for property '${key}', cannot infer a type from a value like '${value}' (${typeof value})`
+)
+```
+
+learned about the `!` operator
+learned about `get`
+learned about enums
+writing tests is a great way to read code
+why would a person use object.assign?
